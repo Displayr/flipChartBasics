d2803ad12687d1bc4d7ba3609700c1d0f6a40c0d
jakehoare <jake_hoare@hotmail.com>
2017-10-27 09:13:09 +1100

    Add DEfault and Automatic to ChartNumberFormat tests

--- tests/testthat/test-chartnumberformat.R
+++ tests/testthat/test-chartnumberformat.R
@@ -1,7 +1,8 @@
 context("ChartNumberFormat")
 
-types <- c("Number", "Percentage", "Date/Time", "Currency", "Metric units suffix", "Scientific", "Custom")
-expected <- c(".3f", ".3%", ".3", ".3f", ".3s", ".3e", ".3")
+types <- c("Number", "Percentage", "Date/Time", "Currency", "Metric units suffix",
+           "Scientific", "Custom", "Automatic", "Category")
+expected <- c(".3f", ".3%", ".3", ".3f", ".3s", ".3e", ".3", "", "Category")
     
 test_that("ChartNumberFormat", {
     
