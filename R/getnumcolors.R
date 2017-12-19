0fc3d899685cd2dcaecd016824b4542010004eb1
jakehoare <jake_hoare@hotmail.com>
2017-12-19 15:24:40 +1100

    DS-90 Stream get colors from rows

--- R/getnumcolors.R
+++ R/getnumcolors.R
@@ -19,30 +19,33 @@ GetNumColors <- function(data, chart.type, scatter.colors.column = 4)
             return(nrow(data[[1]]))
         # Data frame with colors variable
         if (is.null(ncol(data)) || is.na(scatter.colors.column) || ncol(data) < scatter.colors.column || scatter.colors.column <= 0)
-            return(list(num.series=1))
-        return(list(num.series=length(unique(data[,scatter.colors.column]))))
+            return(list(num.series = 1))
+        return(list(num.series = length(unique(data[,scatter.colors.column]))))
     }
     if (chart.type == "Bar Pictograph")
     {
         # Very similar but slightly different for Pie, in the case of a matrix with one column
         if (is.null(dim(data)))
             return(list(num.series = length(data)))
-        else
-            return(list(num.series = nrow(data)))
+        return(list(num.series = nrow(data)))
     }
-    if (grepl("Pie", chart.type) || grepl("Donut", chart.type))
+    if (chart.type == "Pie" || chart.type == "Donut")
     {
         if (!is.null(dim(data)) && length(dim(data)) > 1)
-            return(list(num.series=ncol(data), num.categories=nrow(data)))
-        else
-            return(list(num.series=length(data)))
+            return(list(num.series = ncol(data), num.categories = nrow(data)))
+        return(list(num.series=length(data)))
     }
-    if (grepl("Heat", chart.type) || grepl("Geographic", chart.type))
+    if (chart.type == "Heat" || chart.type == "Geographic")
     {
-        return(list(num.series=10))
+        return(list(num.series = 10))
+    }
+    if (chart.type == "Stream")
+    {
+        if (is.null(dim(data)))
+            return(list(num.series = 1))
+        return(list(num.series = nrow(data)))
     }
     if (!is.null(ncol(data)))
-        return(list(num.series=ncol(data)))
-    else
-        return(list(num.series=1))
+        return(list(num.series = ncol(data)))
+    return(list(num.series = 1))
 }
