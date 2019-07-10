8c5d6fdd694258b7bf976c27e5b790d9afef6105
chschan <chschan@gmail.com>
2019-07-10 15:14:08 +1000

    DS-2453: Allow inputs of different lengths

--- R/matchtable.R
+++ R/matchtable.R
@@ -28,24 +28,21 @@ MatchTable <- function(x,
             ref.names <- rownames(ref.table)
             ref.len <- NROW(ref.table) # this works even if rownames is null
         }
+        if (is.null(ref.names) && !is.null(names(ref.table)))
+            ref.names <- names(ref.table)
     } else
         ref.len <- length(ref.names) 
     x.names <- rownames(x)
     if (is.null(x.names) && !is.null(names(x)))
         x.names <- names(x)
-    if (is.null(ref.names) && !is.null(names(ref.table)))
-        ref.names <- names(ref.table)
+
     x <- as.vector(unlist(x))
   
     # Check length and names 
     if (nchar(x.table.name) > 0)
         x.table.name <- paste0(x.table.name, ": ")
-    if (is.null(x.names) || is.null(ref.names))
-    {
-        if (NROW(x) != ref.len)
-            stop(x.table.name, "Length (", NROW(x), ") does not match ", ref.table.name, " (", ref.len, ").")
-        return(x)
-    }
+    if (is.null(x.names))
+        return(rep(x, length = ref.len))
     if (any(duplicated(x.names)))
         stop(x.table.name, "Names must be unique.")
 
